721a213b7e77f4d8b94faa19381e15c3e6381f55
membri92 <membri_2@hotmail.com>
2022-10-10 14:38:22 +0200

    BillingAddress dataObject created

---
+++ src/test/java/org/selenium/pom/objects/BillingAddress.java
@@ -0,0 +1,59 @@
+package org.selenium.pom.objects;
+
+public class BillingAddress {
+    private String firstName;
+    private String lastName;
+    private String addressLineOne;
+    private String city;
+    private String postalCode;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddressLineOne() {
+        return addressLineOne;
+    }
+
+    public void setAddressLineOne(String addressLineOne) {
+        this.addressLineOne = addressLineOne;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    private String email;
+}
