cb9af0f109daaac817d2eca23066aa345131436f
membri92 <membri_2@hotmail.com>
2022-10-17 21:38:22 +0200

    adding a wait for fix connection reset error

--- src/test/java/org/selenium/pom/base/BaseTest.java
+++ src/test/java/org/selenium/pom/base/BaseTest.java
@@ -17,6 +17,7 @@ public class BaseTest {
 
     @AfterMethod
     public void quitDriver() throws InterruptedException {
+        //fix connection reset error
         Thread.sleep(100);
         driver.quit();
     }
