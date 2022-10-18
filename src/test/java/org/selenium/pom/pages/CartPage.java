b09b6387f001d9bcb8d05a95036651e2966bdc48
membri92 <membri_2@hotmail.com>
2022-10-18 21:38:00 +0200

    cacheLookup annotation added

--- src/test/java/org/selenium/pom/pages/CartPage.java
+++ src/test/java/org/selenium/pom/pages/CartPage.java
@@ -3,6 +3,7 @@ package org.selenium.pom.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
@@ -13,6 +14,8 @@ public class CartPage extends BasePage {
     @FindBy(css = "td[class='product-name'] a")
     private WebElement productName;
     @FindBy(css = ".checkout-button")
+    //this annotation cached an element in cache
+    @CacheLookup
     private WebElement checkoutBtn;
     @FindBy(how = How.CSS, using = ".has-text-align-center")
     private WebElement cartHeading;
