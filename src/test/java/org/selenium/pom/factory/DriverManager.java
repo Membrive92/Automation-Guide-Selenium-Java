4dad9d63c9a5db83f04184f97bb7e23c0f1c87e4
membri92 <membri_2@hotmail.com>
2022-10-20 20:02:36 +0200

    Added WebDrimerManager dependency and added generated folder with drivers

--- src/test/java/org/selenium/pom/factory/DriverManager.java
+++ src/test/java/org/selenium/pom/factory/DriverManager.java
@@ -1,17 +1,16 @@
 package org.selenium.pom.factory;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.time.Duration;
 
 public class DriverManager {
 
     public WebDriver initializerDriver(){
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/driver/chromedriver.exe");
+        WebDriverManager.chromedriver().cachePath("src/test/resources/Drivers").setup();
+       // System.setProperty("webdriver.chrome.driver", "src/test/resources/driver/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-       // driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(25));
         return driver;
     }
 }
