99cbd21408a04ef073432cb3853d71b922f9bb19
membri92 <membri_2@hotmail.com>
2022-10-28 22:14:08 +0200

    Refactoring to Testng

--- src/test/java/org/selenium/pom/base/BaseTest.java
+++ src/test/java/org/selenium/pom/base/BaseTest.java
@@ -1,52 +1,39 @@
 package org.selenium.pom.base;
 
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+
 import org.openqa.selenium.WebDriver;
 import org.selenium.pom.factory.DriverManager;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 
 import java.sql.Driver;
 
 public class BaseTest {
-  //  private ThreadLocal <WebDriver> driver = new ThreadLocal<>();
-    protected WebDriver driver;
-
-  /*  private void setDriver(WebDriver driver){
+    private ThreadLocal <WebDriver> driver = new ThreadLocal<>();
+   private void setDriver(WebDriver driver){
         this.driver.set(driver);
     }
 
     protected WebDriver getDriver(){
       return this.driver.get();
     }
-   /* @Parameters("browser")
+    @Parameters("browser")
     @BeforeMethod
-    //public void startDriver(String browser){*/
-
-
-    @BeforeEach
-    public void startDriver(){
-      //  browser = System.getProperty("browser" , browser);
-        String browser = System.getProperty("browser");
-        driver = new DriverManager().initializerDriver(browser);
-    /*   setDriver(new DriverManager().initializerDriver(browser));
+    public void startDriver(String browser){
+       browser = System.getProperty("browser" , browser);
+       setDriver(new DriverManager().initializerDriver(browser));
         System.out.println("CURRENT THREAD: " + Thread.currentThread().getId() + ", " +
-                "DRIVER = " + getDriver());*/
-        System.out.println("CURRENT THREAD: " + Thread.currentThread().getId() + ", " +
-                "DRIVER = " + driver);
-
+                "DRIVER = " + getDriver());
     }
 
-  //  @AfterMethod
-    @AfterEach
+    @AfterMethod
     public void quitDriver() throws InterruptedException {
         //fix connection reset error
         Thread.sleep(100);
-      /*  System.out.println("CURRENT THREAD: " + Thread.currentThread().getId() + ", " +
+       System.out.println("CURRENT THREAD: " + Thread.currentThread().getId() + ", " +
                 "DRIVER = " + getDriver());
-        getDriver().quit();*/
-        System.out.println("CURRENT THREAD: " + Thread.currentThread().getId() + ", " +
-                "DRIVER = " + driver);
-        driver.quit();
+        getDriver().quit();
     }
 }
--- src/test/java/org/selenium/pom/tests/MyFirstTestCase.java
+++ src/test/java/org/selenium/pom/tests/MyFirstTestCase.java
@@ -1,9 +1,6 @@
 package org.selenium.pom.tests;
 
 
-
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.selenium.pom.base.BaseTest;
 import org.selenium.pom.objects.BillingAddress;
 import org.selenium.pom.objects.Product;
@@ -13,6 +10,8 @@ import org.selenium.pom.pages.CheckoutPage;
 import org.selenium.pom.pages.HomePage;
 import org.selenium.pom.pages.StorePage;
 import org.selenium.pom.utils.JacksonUtils;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 
 import java.io.IOException;
@@ -25,31 +24,25 @@ public class MyFirstTestCase extends BaseTest {
       BillingAddress  billingAddress = JacksonUtils.deserializeJson("myBillingAddress.json", BillingAddress.class);
       Product product = new Product(1215);
 
-      //StorePage storePage = new HomePage(getDriver()).
-        StorePage storePage = new HomePage(driver).
+      StorePage storePage = new HomePage(getDriver()).
                 load().
                 navigateToStoreUsingMenu();
       storePage.isLoaded();
       storePage.search(searchFor);
-      //  Assert.assertTrue(storePage.getSearchResultTitle().contains("Search results: "));
-        Assertions.assertEquals("Search results: “" + searchFor + "”", storePage.getSearchResultTitle());
+        Assert.assertTrue(storePage.getSearchResultTitle().contains("Search results: "));
         
         storePage.clickAddToCardBtn(product.getName());
         CartPage cartPage = storePage.clickViewCart();
         cartPage.isLoaded();
-   //     Assert.assertEquals(cartPage.getProductName(), product.getName());
-        Assertions.assertEquals(product.getName(), cartPage.getProductName());
-
+       Assert.assertEquals(cartPage.getProductName(), product.getName());
         CheckoutPage checkoutPage = cartPage.
                 checkout().
                 setBillingAddress(billingAddress).
                 selectDirectBankTransfer().
                 placeOrder();
-  //      Assert.assertEquals(checkoutPage.getNotice(), "Thank you. Your order has been received.");
-        Assertions.assertEquals("Thank you. Your order has been received.", checkoutPage.getNotice());
+       Assert.assertEquals(checkoutPage.getNotice(), "Thank you. Your order has been received.");
     }
 
-  //  @Test
     @Test
     public void loginAndCheckoutUsingDirectBankTransfer() throws InterruptedException, IOException {
         String searchFor = "blue";
@@ -57,18 +50,15 @@ public class MyFirstTestCase extends BaseTest {
         Product product = new Product(1215);
         User user = new User("userfk1","userfk2");
 
-        //StorePage storePage = new HomePage(getDriver()).
-        StorePage storePage = new HomePage(driver).
+        StorePage storePage = new HomePage(getDriver()).
                 load().
                 navigateToStoreUsingMenu().
                 search(searchFor);
-      //  Assert.assertTrue(storePage.getSearchResultTitle().contains("Search results: "));
-        Assertions.assertEquals("Search results: “" + searchFor + "”", storePage.getSearchResultTitle());
+       Assert.assertTrue(storePage.getSearchResultTitle().contains("Search results: "));
 
         storePage.clickAddToCardBtn(product.getName());
         CartPage cartPage = storePage.clickViewCart();
-      //  Assert.assertEquals(cartPage.getProductName(), (product.getName()));
-        Assertions.assertEquals(product.getName(), cartPage.getProductName());
+        Assert.assertEquals(cartPage.getProductName(), (product.getName()));
 
         CheckoutPage checkoutPage = cartPage.checkout();
         checkoutPage.clickHereToLoginLink();
@@ -78,7 +68,6 @@ public class MyFirstTestCase extends BaseTest {
                    setBillingAddress(billingAddress).
                    selectDirectBankTransfer().
                    placeOrder();
-       // Assert.assertEquals(checkoutPage.getNotice(), "Thank you. Your order has been received.");
-        Assertions.assertEquals("Thank you. Your order has been received.", checkoutPage.getNotice());
+        Assert.assertEquals(checkoutPage.getNotice(), "Thank you. Your order has been received.");
     }
 }
