c6e0ae59a6f2cf60b0bcd4b224f37a951e09314f
membri92 <membri_2@hotmail.com>
2022-11-01 12:47:13 +0100

    Atomic Tests: Creating AddToCartTest class and test

---
+++ src/test/java/org/selenium/pom/tests/AddToCartTest.java
@@ -0,0 +1,24 @@
+package org.selenium.pom.tests;
+
+
+import org.selenium.pom.base.BaseTest;
+import org.selenium.pom.objects.Product;
+import org.selenium.pom.pages.CartPage;
+import org.selenium.pom.pages.StorePage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class AddToCartTest extends BaseTest {
+
+    @Test
+    public void addToCartFromStorePage() throws IOException {
+        Product product = new Product(1215);
+        CartPage cartPage = new StorePage(getDriver()).
+                load().
+                clickAddToCardBtn(product.getName()).
+                clickViewCart();
+        Assert.assertEquals(cartPage.getProductName(), (product.getName()));
+    }
+}
