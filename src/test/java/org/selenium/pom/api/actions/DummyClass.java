2695bdfa63ce38e2de568d460e5c99590b8a29ee
membri92 <membri_2@hotmail.com>
2022-11-02 20:11:47 +0100

    Api Integration: Creating class for get account Api

---
+++ src/test/java/org/selenium/pom/api/actions/DummyClass.java
@@ -0,0 +1,8 @@
+package org.selenium.pom.api.actions;
+
+public class DummyClass {
+
+    public static void main(String[] args){
+        new SingUpApi().getAccount();
+    }
+}
---
+++ src/test/java/org/selenium/pom/api/actions/SingUpApi.java
@@ -0,0 +1,33 @@
+package org.selenium.pom.api.actions;
+
+import io.restassured.http.Cookies;
+import io.restassured.response.Response;
+import org.selenium.pom.utils.ConfigLoader;
+
+import static io.restassured.RestAssured.given;
+
+public class SingUpApi {
+    private Cookies cookies;
+
+    public Cookies getCookies(){
+        return cookies;
+    }
+
+    public Response getAccount(){
+        Cookies cookies = new Cookies();
+        Response response = given().
+                baseUri(ConfigLoader.getInstance().getBaseUrl()).
+                cookies(cookies).
+                log().all().
+         when().
+                    get("/account").
+         then().
+                    log().all().
+                    extract().
+                    response();
+        if (response.getStatusCode() != 200){
+            throw new RuntimeException("Failed to fetch the account, HTTP Status Code: " + response.getStatusCode());
+        }
+        return response;
+    }
+}
