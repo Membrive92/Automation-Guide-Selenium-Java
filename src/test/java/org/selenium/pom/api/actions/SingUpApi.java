0529e07760f5c51618369ddee6187b52f848990a
membri92 <membri_2@hotmail.com>
2022-11-03 17:06:19 +0100

    Api Integration: Post account Api

--- src/test/java/org/selenium/pom/api/actions/SingUpApi.java
+++ src/test/java/org/selenium/pom/api/actions/SingUpApi.java
@@ -1,11 +1,16 @@
 package org.selenium.pom.api.actions;
 
 import io.restassured.http.Cookies;
+import io.restassured.http.Header;
+import io.restassured.http.Headers;
 import io.restassured.response.Response;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.selenium.pom.objects.User;
 import org.selenium.pom.utils.ConfigLoader;
+
+import java.util.HashMap;
 
 import static io.restassured.RestAssured.given;
 
@@ -16,19 +21,19 @@ public class SingUpApi {
         return cookies;
     }
 
-    public String fetchRegisterNonceValueUsingGroovy(){
+    private String fetchRegisterNonceValueUsingGroovy(){
         Response response = getAccount();
        return response.htmlPath().getString("**.findAll { it.@name == 'woocommerce-register-nonce' }.@value");
     }
 
-    public String fetchRegisterNonceValueUsingJSoup(){
+    private String fetchRegisterNonceValueUsingJSoup(){
         Response response = getAccount();
         Document doc = Jsoup.parse(response.body().prettyPrint());
         Element element = doc.selectFirst("#woocommerce-register-nonce");
         return element.attr("value");
     }
 
-    public Response getAccount(){
+    private Response getAccount(){
         Cookies cookies = new Cookies();
         Response response = given().
                 baseUri(ConfigLoader.getInstance().getBaseUrl()).
@@ -42,6 +47,34 @@ public class SingUpApi {
                     response();
         if (response.getStatusCode() != 200){
             throw new RuntimeException("Failed to fetch the account, HTTP Status Code: " + response.getStatusCode());
+        }
+        return response;
+    }
+
+    public Response register(User user){
+        Cookies cookies = new Cookies();
+        Header header = new Header("Content-Type", "application/x-www-form-urlencoded");
+        Headers headers = new Headers(header);
+        HashMap<String, String> formParams = new HashMap<>();
+        formParams.put("username", user.getUsername());
+        formParams.put("email", user.getEmail());
+        formParams.put("woocommerce-register-nonce", fetchRegisterNonceValueUsingJSoup());
+        formParams.put("register", "Register");
+
+        Response response = given().
+                baseUri(ConfigLoader.getInstance().getBaseUrl()).
+                headers(headers).
+                body(formParams).
+                cookies(cookies).
+                log().all().
+                when().
+        post("/account").
+                then().
+                log().all().
+                extract().
+                response();
+        if (response.getStatusCode() != 302){
+            throw new RuntimeException("Failed to register the account, HTTP Status Code: " + response.getStatusCode());
         }
         return response;
     }
--- src/test/java/org/selenium/pom/objects/User.java
+++ src/test/java/org/selenium/pom/objects/User.java
@@ -4,10 +4,14 @@ public class User {
 
     private String username;
     private String password;
+    private String email;
 
-    public User(String username, String password){
+
+
+    public User(String username, String password, String email){
         this.username = username;
         this.password = password;
+        this.email = email;
     }
 
     public String getUsername() {
@@ -25,5 +29,14 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
 
 }
