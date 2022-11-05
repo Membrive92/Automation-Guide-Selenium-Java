a05214171d9d98a5f27d6cfa656d2b6b17d1e60e
membri92 <membri_2@hotmail.com>
2022-11-05 13:34:15 +0100

    Method for inject cookie to broser created

--- src/test/java/org/selenium/pom/base/BaseTest.java
+++ src/test/java/org/selenium/pom/base/BaseTest.java
@@ -2,14 +2,18 @@ package org.selenium.pom.base;
 
 
 
+import io.restassured.http.Cookies;
+import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
 import org.selenium.pom.factory.DriverManager;
+import org.selenium.pom.utils.CookieUtils;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
 import java.sql.Driver;
+import java.util.List;
 
 public class BaseTest {
     private ThreadLocal <WebDriver> driver = new ThreadLocal<>();
@@ -37,5 +41,13 @@ public class BaseTest {
        System.out.println("CURRENT THREAD: " + Thread.currentThread().getId() + ", " +
                 "DRIVER = " + getDriver());
         getDriver().quit();
+    }
+
+    public void injectCookiesToBrowser(Cookies cookies){
+        List<Cookie> seleniumCookies = new CookieUtils().convertRestAssuredCookiesToSeleniumCookies(cookies);
+        for (Cookie cookie: seleniumCookies){
+            getDriver().manage().addCookie(cookie);
+        }
+
     }
 }
